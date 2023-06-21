2f62dccff7242dc7df7ace78e564bebe85c79f5c
Brenda Vieira <brendanascimentovieirah@gmail.com>
2023-06-21 11:16:05 -0400

    commit

--- script.js
+++ script.js
@@ -13,45 +13,47 @@ class Produto {
         this.preco = preco
     }
 
-    mostrar_produtos(){
-        return {
-            nome: this.nome,
-            dataCadastro: this.dataCadastro,
-            descricao: this.descricao,
-            preco: this.preco
-      }
-   }
-}
-
- class ProdutosDestaque extends Produto {
-    constructor(){
-        super(nome, dataCadastro, descricao, preco, Image)
-    }
-
-    retornar_mostrarProdutos() {
+    retornar_mostrarrodutos() {
         try{
-         return this.mostrar_produtosDestaque();   
+         return this.mostrar_produtos();   
         } catch (error) {
             return error
         }
     }
 
-    mostrar_produtoDestaque(){
-        if(this.nome != "" && this.dataCadastro != "" && this.descricao != "" && this.preco != "" && this.image != ""){
-            return {
-                nome: this.nome,
-                dataCadastro: this.dataCadastro,
-                descricao: this.descricao,
-                preco: this.preco,
-                Image: this.image
-          }
+    mostrar_produtos(){
+     if(this.nome != "" && this.dataCadastro != "" && this.descricao != "" && this.preco != ""){
+         return `<div>
+            <h1>${this.nome}</h1>
+            <p>${this.dataCadastro}</p>
+            <h2>${this.descricao}</h2>
+            <h2>${this.preco}</h2></div>`
          } else {
             throw new Erro("Há atributo vazio.")
-        }
-      }
- }
+     }
+  }
+}
+ class ProdutosDestaque extends Produto {
+    constructor(){
+        super(nome, dataCadastro, descricao, preco, imageDest)
+    }
 
- const produtoDois = new Produto("Photocards", "27/06", "Photocard aleatório", "40", "");
- console.log(produtoDois.retornar_mostrarProdutos());
+    mostrar_produtoDestaque(){
+            return `<div>${this.nome}</div>
+          <div>${this.dataCadastro}</div>
+          <div>${this.descricao}</div>
+          <div>${this.preco}</div>
+          <div>${this.imageDest}</div>`
+          }
+         } 
+ 
 
-//faltando um conteúdo, pois não foi compreendido//
+ //const produto = new ProdutoDestaque ("Album Face", "03-05","Album Kpop","150,00","www.imagenspng.com.br/wp-content/uploads/2022/04/flork-png-08-600x568.png")
+ const produtoDois = new Produto("Photocards", "27/06","Photocards aleatórios","40,00");
+ //git init//
+ 
+ //let produtosDest = document.getElementById("produto-destaque")
+ let produtos = document.getElementById("lista-produtos");
+ 
+ //produtosDest.insertAdjacentHTML('afterbegin', 'mostrar_produtos_destaque');
+ produtos.insertAdjacentHTML('afterbegin', produtoDois.mostrar_produtos());
